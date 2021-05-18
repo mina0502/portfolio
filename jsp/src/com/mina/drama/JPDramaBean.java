a3bc1f08215bee5c0dfe515f55905f8d9624e655
mina0502 <54879715+mina0502@users.noreply.github.com>
2021-05-18 12:46:15 +0900

    Add files via upload

---
+++ jsp/src/com/mina/drama/JPDramaBean.java
@@ -0,0 +1,70 @@
+package com.mina.drama;
+
+public class JPDramaBean {
+	private String jp_filename;
+	private String jp_code;
+	private String jp_name;
+	private String jp_bu;
+	private String jp_description;
+	private String jp_genre;
+	private String jp_broad;
+	private String jp_person;
+	private String jp_year;
+
+
+	public String getJp_filename() {
+		return jp_filename;
+	}
+	public void setJp_filename(String jp_filename) {
+		this.jp_filename = jp_filename;
+	}
+	public String getJp_code() {
+		return jp_code;
+	}
+	public void setJp_code(String jp_code) {
+		this.jp_code = jp_code;
+	}
+	public String getJp_name() {
+		return jp_name;
+	}
+	public void setJp_name(String jp_name) {
+		this.jp_name = jp_name;
+	}
+	public String getJp_bu() {
+		return jp_bu;
+	}
+	public void setJp_bu(String jp_bu) {
+		this.jp_bu = jp_bu;
+	}
+	public String getJp_description() {
+		return jp_description;
+	}
+	public void setJp_description(String jp_description) {
+		this.jp_description = jp_description;
+	}
+	public String getJp_genre() {
+		return jp_genre;
+	}
+	public void setJp_genre(String jp_genre) {
+		this.jp_genre = jp_genre;
+	}
+	public String getJp_broad() {
+		return jp_broad;
+	}
+	public void setJp_broad(String jp_broad) {
+		this.jp_broad = jp_broad;
+	}
+	public String getJp_person() {
+		return jp_person;
+	}
+	public void setJp_person(String jp_person) {
+		this.jp_person = jp_person;
+	}
+	public String getJp_year() {
+		return jp_year;
+	}
+	public void setJp_year(String jp_year) {
+		this.jp_year = jp_year;
+	}
+	
+}
